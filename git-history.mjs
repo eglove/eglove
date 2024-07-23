c542f964d072043cdf5bf8110978ee2913748759
Ethan Glover <hello@ethang.email>
2024-07-23 11:25:16 -0500

    update script

--- git-history.mjs
+++ git-history.mjs
@@ -1,49 +1,36 @@
 import { simpleGit } from "simple-git";
-import fs from "node:fs";
+import { DateTime } from "luxon";
+import { writeFile } from "node:fs";
+import { exec } from "node:child_process";
 
 const dir = "C:\\Users\\hello\\Projects\\ethang\\eglove";
+const gitFormat = "yyyy-MM-dd HH:mm";
 
 process.chdir(dir);
+
+let start = DateTime.now().minus({ year: 1 });
+const end = DateTime.now();
 
 const git = simpleGit({
   baseDir: dir,
 });
-const log = await git.log();
 
-const start = new Date();
-start.setFullYear(start.getFullYear() - 1);
+while (end.diff(start, "day").values.days > 0) {
+  const result = await exec(
+    `git log --after="${start.toISO()}" --before="${end.toISO()}"`,
+  );
 
-const end = new Date();
-
-let found = 0;
-let missing = 0;
-
-console.log(log.latest);
-
-while (start <= end) {
-  const foundCommit = log.all.find((item) => {
-    return (
-      new Date(item.date).toLocaleDateString() === start.toLocaleDateString()
-    );
-  });
-
-  if (foundCommit) {
-    found += 1;
-  } else {
-    missing += 1;
+  if (result.toString() === "") {
     console.log(`Commiting for ${start.toLocaleString()}.`);
-    fs.writeFileSync("fake-history.txt", start.toISOString());
+    await writeFile("fake-history.txt", start.toISO());
     await git.add(".");
     await git.commit("Hmm...", {
-      "--date": start.toISOString(),
+      "--date": start.toISO(),
     });
   }
 
-  start.setDate(start.getDate() + 1);
+  start = start.plus({ day: 1 });
 }
 
 await git.push();
-
-console.log(`${found} commits found`);
-console.log(`${missing} commits created`);
 process.exit();
